d3b519b0140e63957302ce22d4966b037b7efd2d
Amangarala <103805887+Amangarala@users.noreply.github.com>
2023-08-10 22:51:43 +0530

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -3,13 +3,13 @@ import 'package:in_app_chat/popup_menu_button.dart';
 import 'package:zego_zimkit/compnents/conversation_list.dart';
 import 'package:zego_zimkit/pages/message_list_page.dart';
 
-// ZIMKit().connectUser(id: id, name: name).then(_){
-//   Navigator.of(context).push(
-//     MaterialPageRoute(
-//       builder: (context) => const MyHomePage(),
-//     ),
-//   );
-// }
+ZIMKit().connectUser(id: id, name: name).then(_){
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => const MyHomePage(),
+    ),
+  );
+}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
